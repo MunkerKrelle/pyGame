e66d9096cea2a34496bc9e3306d53e85706dcb08
JeppeKjeldal <jeppekjeldal95@gmail.com>
2025-02-24 13:36:32 +0100

    Revert "Merge pull request #11 from MunkerKrelle/Jeppe-Menu"
    This reverts commit 48550d0e7dab33942be6126b39fd00ed059a6981, reversing
    changes made to 3101f39a3c2e5a577dbcf677b9e2b14b15c9dc2c.

--- pyGame/GameWorld.py
+++ pyGame/GameWorld.py
@@ -5,9 +5,6 @@ from Components import SpriteRenderer
 from Player import Player
 from Builder import PlayerBuilder
 from Builder import EnemyBuilder
-from ScoreManager import ScoreManager
-
-
 class GameWorld:
 
     def __init__(self) -> None:
@@ -40,10 +37,6 @@ class GameWorld:
         self._running = True
         self._clock = pygame.time.Clock()
 
-        self.score_manager = ScoreManager()
-        self.player = Player()
-        self.font = pygame.font.SysFont("Arial", 30)
-
     @property
     def screen(self):
         return self._screen
@@ -70,36 +63,13 @@ class GameWorld:
 
         while self._running:
             for event in pygame.event.get():
-
                 if event.type == pygame.QUIT:
                     self._running =False
-    
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_o:
-                        self.score_manager.increase_score()
-
-                    if event.key == pygame.K_p:
-                        self.score_manager.decrease_score()
-
-                    if self.score_manager.score >= 10:
-                        self.show_endgame_screens()
-                        #Virker ikke, but who cares? --> self.score_manager.score = 0
-
-                    if event.key == pygame.K_q:
-                        self.player._lives -= 1
-
-                    if self.player._lives <= 0:
-                        self.show_gameover_screen()
-
-            
-
 
             self._screen.fill("cornflowerblue")
 
-            self.draw_lives()
-
             delta_time = self._clock.tick(60) / 1000.0
-            
+
             #draw your game
             for gameObject in self._gameObjects[:]:
                 gameObject.update(delta_time)
@@ -112,44 +82,16 @@ class GameWorld:
             self._gameObjects = [obj for obj in self._gameObjects if not obj.is_destroyed]
             self._colliders = [col for col in self._colliders if not col.gameObject.is_destroyed]
 
-
-            self.score_manager.draw(self._screen)
-
             pygame.display.flip()
             self._clock.tick(60)
 
-    def show_endgame_screens(self):
-        from EndGameScreen import CompletedLevelScreen
-        self.completed_screen = CompletedLevelScreen()
-        self.completed_screen.run()
-
-    def show_gameover_screen(self):
-        from EndGameScreen import GameOverScreen
-        self.gameover_screen = GameOverScreen()
-        self.gameover_screen.run()
-
-
-    def draw_lives(self):
-        x_pos = 10
-        y_pos = 30
-
-        lives_text = self.font.render(f"Lives: {self.player.lives}", True, (255,255,255))
-        self._screen.blit(lives_text, (x_pos, y_pos))
-
-    def run(self):
-        gw.Awake()
-        gw.Start()
-
-
-
-        while self._running:
-            gw.update()
-            
         pygame.quit()
     
 
 gw = GameWorld()
 
-
+gw.Awake()
+gw.Start()
+gw.update()
 
         
